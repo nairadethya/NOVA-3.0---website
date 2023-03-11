d9f4a3dd0ec5e5e3ec29a8be5137cff6ef05992f
Adethya Nair <adethyasanjay@gmail.com>
2023-03-11 17:56:35 +0530

    an unwanted file was removed

--- fastapi-postgres-project/main.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-import databases
-import sqlalchemy
-from fastapi import FastAPI, status
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import os
-import urllib
